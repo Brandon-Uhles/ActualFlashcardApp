1cb618b62c0a00864ae50b20389a449fe55efd74
Brandon-Uhles <brandon.uhles@gmail.com>
2022-03-29 23:13:46 -0400

    Added some core dependencies for the project that contain the constructor and useful methods for both the Flashcards and Flashcard Sets. Also added a utility class for future JSON implementation.

---
+++ CoreDependencies/JSONHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualFlashcardApp.CoreDependencies
+{
+    public static class JSONHelper
+    {
+        /// Filter for windows filetypes when saving or opening a new file
+        public const string DialogFilter = "JSON Files (*.json)|*.json|All files (*.*)|*.*";
+
+        /// Sets the property name for the flashcard list
+        public const string FlashcardsListJsonPropertyName = "flashcardsList";
+
+        /// Sets the property name for the set of flashcards
+        public const string SetNameJsonPropertyName = "setName";
+
+        /// Sets the property name for one side of the flashcard
+        public const string TermJsonPropertyName = "term";
+
+        /// Sets the property name for the other side of the flashcard
+        public const string DefinitionJsonPropertyName = "definition";
+    }
+}
